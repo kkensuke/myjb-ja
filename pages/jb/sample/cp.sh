f98df688668301d9de2a30821a3e190f1e0bdfda
kkensuke <n4659017382@gmail.com>
2023-12-10 20:10:00 +0900

    add

---
+++ pages/jb/sample/cp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -u
+
+
+mkdir -p ~/My\ Drive/app/github/jb-public/myjb-ja/_build/html/pages/jb/sample
+
+ln -sf ~/My\ Drive/app/github/jb-public/myjb-ja/pages/jb/sample/test.png ~/My\ Drive/app/github/jb-public/myjb-ja/_build/html/pages/jb/sample/test.png
+ln -sf ~/My\ Drive/app/github/jb-public/myjb-ja/pages/jb/sample/test.pdf ~/My\ Drive/app/github/jb-public/myjb-ja/_build/html/pages/jb/sample/test.pdf
